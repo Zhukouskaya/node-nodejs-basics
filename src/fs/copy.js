ee2c75ae17498c2bdca4cd063a8d250bf24613f1
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-12-02 14:05:54 +0300

    feat: task fs/copy is done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,13 @@
-const copy = async () => {
-    // Write your code here 
+import { readdir, mkdir, copyFile } from 'fs/promises';
+
+export const copy = async () => {
+    try {
+        await mkdir('files_copy');
+        const readDir = await readdir('files')
+        readDir.map(file => copyFile(`files/${file}`, `files_copy/${file}`))
+    } catch (err) {
+        throw new Error ('FS operation failed')
+    }
 };
 
 copy();
