cf239f9c0f92cc1309f0bb61424266a50dbaf9f1
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-12-02 14:54:04 +0300

    feat: task fs/read is done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,11 @@
-const read = async () => {
-    // Write your code here 
+import { readFile } from 'fs/promises';
+
+export const read = async () => {
+    try {
+        console.log(await readFile('files/fileToRead.txt', 'utf8'))
+    } catch (err) {
+        throw new Error ('FS operation failed')
+    }
 };
 
-await read();
+read();
