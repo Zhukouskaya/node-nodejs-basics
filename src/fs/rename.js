8ab90bb8b29ce0fcf1d483b4205c48edd44b1dea
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-12-02 14:22:56 +0300

    feat: task fs/rename is done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,11 @@
-const rename = async () => {
-    // Write your code here 
+import { rename as renameFile } from 'fs/promises';
+
+export const rename = async () => {
+    try {
+        await renameFile ('files/wrongFilename.txt', 'files/properFilename.md') 
+    } catch (err) {
+        throw new Error ('FS operation failed')
+    }
 };
 
-await rename();
+rename();
