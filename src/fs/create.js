4aa45202b86afd62113a64b8fc808c284e9b6dc8
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-12-02 13:21:21 +0300

    feat: task fs/create is done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile } from 'fs/promises';
+
+export const create = async () => {
+    try {
+        await writeFile('files/fresh.txt',  'I am fresh and young', { flag: 'wx' });
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
-await create();
+create();
