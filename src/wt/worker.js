e9d3b99c2109ec3f2db115d72f0dab2ab47ac43f
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-12-05 16:42:38 +0300

    feat: task wt/main is done

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,6 @@
-import { workerData, parentPort, MessageChannel } from 'worker_threads';
+import { workerData, parentPort } from 'worker_threads';
 
-export const nthFibonacci = () => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
     parentPort.postMessage(nthFibonacci(workerData))
