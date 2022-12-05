b5d21f06582f710b2ef3236747bf73bacd0457fd
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-12-05 14:38:48 +0300

    feat: task zip/decompress is done

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,11 @@
-const decompress = async () => {
-    // Write your code here 
+import { createUnzip } from 'zlib'
+import { createReadStream, createWriteStream } from 'fs'; 
+
+export const decompress = async () => {
+    const read = createReadStream('src/zip/files/archive.gz')
+    const write = createWriteStream( 'src/zip/files/fileToCompress.txt')
+
+    read.pipe(createUnzip()).pipe(write)
 };
 
-await decompress();
+decompress();
