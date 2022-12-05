2b4dd6625ad1ad846e8c200cde4e54f78fcfad4b
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-12-05 22:38:17 +0300

    feat: task CP is done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'child_process'
+import process from 'process';
+
+export const spawnChildProcess = async (args) => {
+    const cd = spawn('node', ['src/cp/files/script.js', ...args])
+
+    process.stdin.on('data', (msg) => {
+        cd.stdin.write(msg);
+    })
+
+    cd.stdout.on('data', (data) => { 
+        console.log(data.toString())
+    })
+
+    
 };
 
-spawnChildProcess();
+spawnChildProcess('a', 'b', 'c');
