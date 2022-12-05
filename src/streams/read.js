73dfbabb18a3b90125690d064e7f3f7abe022d6e
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-12-05 13:14:41 +0300

    feat: task streams/read is done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
-const read = async () => {
-    // Write your code here 
+import { createReadStream } from 'fs'
+
+export const read = async () => {
+    const read = createReadStream('src/streams/files/filetoRead.txt', 'utf-8')
+    read.on('data', (data) => {
+        process.stdout.write(data)
+    })
 };
 
-await read();
+read();
